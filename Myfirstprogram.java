ce3216b7a3e9a0c466df6969a6e98691729ad28e
charbel el khoury <charbel76107@gmail.com>
2023-01-25 20:20:19 +0200

    My

---
+++ Myfirstprogram.java
@@ -0,0 +1,9 @@
+public class Myfirstprogram
+{
+       public static void main(String[] args){
+       System.out.print("this is my ");
+       System.out.println("first java program");
+       System.out.println("programming is fun!");
+}
+
+}
